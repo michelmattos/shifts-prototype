f5cc4f49219ff1e0f26f019564f7c2debfcb08e6
Michel Mattos <7696411+michelmattos@users.noreply.github.com>
2021-09-12 12:24:35 +1200

    Added design tokens for theme and font

--- components/ThemeProvider/theme.ts
+++ components/ThemeProvider/theme.ts
@@ -1,19 +1,40 @@
 import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const fonts = { mono: `'Menlo', monospace` }
+const colors = {
+  brand: {
+    lightGrey: '#fafbfc',
+    mediumGrey: '#e0e8ee',
+    darkGrey: '#2f313a',
+    red: '#d0021b',
+    yellow: '#f8e71c',
+  },
+}
 
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+const textStyles = {
+  'roboto-regular': {
+    fontFamily: 'Roboto',
+    fontSize: '12px',
+    fontWeight: '400',
+    lineHeight: '167%',
+    letterSpacing: '0',
+  },
+  'roboto-medium': {
+    fontFamily: 'Roboto',
+    fontSize: '12px',
+    fontWeight: '500',
+    lineHeight: '167%',
+    letterSpacing: '0',
+  },
+  'roboto-bold': {
+    fontFamily: 'Roboto',
+    fontSize: '10px',
+    fontWeight: '700',
+    lineHeight: '200%',
+    letterSpacing: '0',
+  },
+}
 
 export const theme = extendTheme({
-  colors: {
-    black: '#16161D',
-  },
-  fonts,
-  breakpoints,
+  colors,
+  textStyles,
 })
